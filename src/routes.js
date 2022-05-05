f978d0aed7e781e8a6e6a0b3b39f54137c45d392
Demin Dmitry <heaven7pro@gmail.com>
2022-05-05 17:03:42 +0300

    Remove useless frontend routes

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,4 @@ export default {
   loginPath: () => [host, prefix, 'login'].join('/'),
   signupPath: () => [host, prefix, 'signup'].join('/'),
   dataPath: () => [host, prefix, 'data'].join('/'),
-  channelsPath: () => [host, prefix, 'channels'].join('/'),
-  channelPath: (id) => [host, prefix, 'channels', id].join('/'),
-  channelMessagesPath: (id) => [host, prefix, 'channels', id, 'messages'].join('/'),
 };
