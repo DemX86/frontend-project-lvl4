f43d848a44a750cd579fffa31e3b2a6c2ab887cb
Demin Dmitry <heaven7pro@gmail.com>
2022-05-25 18:03:02 +0300

    Refactor modal content components usage

--- src/components/ModalWindow.jsx
+++ src/components/ModalWindow.jsx
@@ -2,26 +2,32 @@ import React from 'react';
 import { Modal } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 
+import AddChannelModalContent from './modalContents/addChannel.jsx';
+import RenameChannelModalContent from './modalContents/renameChannel.jsx';
+import RemoveChannelModalContent from './modalContents/removeChannel.jsx';
 import { modalActions } from '../slices/modalDataSlice.js';
-import getModalContent from './modalContents/getModalContent.js';
 import selectors from '../slices/selectors.js';
+
+const mappingModalComponents = {
+  addChannel: AddChannelModalContent,
+  renameChannel: RenameChannelModalContent,
+  removeChannel: RemoveChannelModalContent,
+};
 
 const ModalWindow = () => {
   const dispatch = useDispatch();
   const isModalActive = useSelector(selectors.selectModalActivity);
   const modalType = useSelector(selectors.selectModalType);
 
-  if (!isModalActive) {
-    return null;
-  }
+  const ModalContent = mappingModalComponents[modalType];
 
   const handleCloseModal = () => {
     dispatch(modalActions.hideModal({}));
   };
 
   return (
-    <Modal show onHide={handleCloseModal}>
-      {React.createElement(getModalContent(modalType), { handleCloseModal })}
+    <Modal show={isModalActive} onHide={handleCloseModal}>
+      {ModalContent && <ModalContent handleCloseModal={handleCloseModal} />}
     </Modal>
   );
 };
